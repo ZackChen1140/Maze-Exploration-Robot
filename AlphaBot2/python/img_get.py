e94698a613c2cb1542800fca6b1570573b5e8947
unknown <asd89324@gmail.com>
2024-05-30 16:42:22 +0800

    test

--- AlphaBot2/python/img_get.py
+++ AlphaBot2/python/img_get.py
@@ -1,49 +1,37 @@
-
-import cv2
-from threading import Thread
-import uuid
-import os
 import time
-count = 0
-def image_collect(cap):
-    global count
+import threading
+from picamera2 import Picamera2, Preview
+
+def capture_image(picamera, interval=1):
+    counter = 0
     while True:
-        success, img = cap.read()
-        if success:
-            file_name = str(uuid.uuid4())+'.jpg'
-            cv2.imwrite(os.path.join('images',file_name),img)
-            count = count+1
-            print("save %d %s"%(count,file_name))
-        time.sleep(0.4)
- 
+        time.sleep(interval)
+        picamera.capture_file(f"images/image_{counter}.jpg")
+        counter += 1
+
+def main():
+    picamera = Picamera2()
+    video_config = picamera.create_video_configuration(main={"size": (1920, 1080)})
+    picamera.configure(video_config)
+
+    picamera.start_preview(Preview.QTGL)
+    
+    # Start the image capture thread
+    capture_thread = threading.Thread(target=capture_image, args=(picamera,))
+    capture_thread.start()
+
+    # Start recording video
+    picamera.start_recording("video.h264")
+
+    # Record for 10 seconds
+    time.sleep(10)
+
+    # Stop recording video
+    picamera.stop_recording()
+    picamera.stop_preview()
+
+    # Ensure the capture thread is stopped (optional)
+    capture_thread.join()
+
 if __name__ == "__main__":
-    
-    os.makedirs("images",exist_ok=True)
-    
-    # 打开摄像头
-    cap = cv2.VideoCapture(0)
- 
-    m_thread = Thread(target=image_collect, args=([cap]),daemon=True)
-    
-    while True:
- 
-        # 读取一帧图像
- 
-        success, img = cap.read()
- 
-        if not success:
- 
-            continue
- 
-        cv2.imshow("video",img)
- 
-        key =  cv2.waitKey(1) & 0xFF   
- 
-        # 按键 "q" 退出
-        if key ==  ord('c'):
-            m_thread.start()
-            continue
-        elif key ==  ord('q'):
-            break
- 
-    cap.release()
+    main()
