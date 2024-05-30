beb08936e76bd893cfe92d8f185def39b2b29ed7
unknown <asd89324@gmail.com>
2024-05-30 14:46:06 +0800

    test

--- AlphaBot2/python/img_get.py
+++ AlphaBot2/python/img_get.py
@@ -47,8 +47,3 @@ if __name__ == "__main__":
             break
  
     cap.release()
-————————————————
-
-                            版权声明：本文为博主原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接和本声明。
-                        
-原文链接：https://blog.csdn.net/black_sneak/article/details/131374492
