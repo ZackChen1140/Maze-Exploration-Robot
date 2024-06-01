c4e6c4362b577d3f02e7aaa95566cd3888ba5084
unknown <asd89324@gmail.com>
2024-06-01 12:46:05 +0800

    test

--- AlphaBot2/python/img_get.py
+++ AlphaBot2/python/img_get.py
@@ -1,5 +1,4 @@
-import time
-import threading
+import cv2
 from picamera2 import Picamera2, Preview
 
 def capture_image(picamera, interval=1):
@@ -11,27 +10,13 @@ def capture_image(picamera, interval=1):
 
 def main():
     picamera = Picamera2()
-    video_config = picamera.create_video_configuration(main={"size": (1920, 1080)})
-    picamera.configure(video_config)
+    picamera.configure(picamera.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
+    picamera.start()
 
-    picamera.start_preview(Preview.QTGL)
-    
-    # Start the image capture thread
-    capture_thread = threading.Thread(target=capture_image, args=(picamera,))
-    capture_thread.start()
-
-    # Start recording video
-    picamera.start_recording("video.h264")
-
-    # Record for 10 seconds
-    time.sleep(10)
-
-    # Stop recording video
-    picamera.stop_recording()
-    picamera.stop_preview()
-
-    # Ensure the capture thread is stopped (optional)
-    capture_thread.join()
+    while True:
+        im = picamera.capture_array()
+        cv2.imshow("Camera", im)
+        cv2.waitKey(1)
 
 if __name__ == "__main__":
     main()
