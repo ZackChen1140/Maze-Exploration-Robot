d9354f9df159f37d3387c77caad90bf2d6955b3e
unknown <asd89324@gmail.com>
2024-06-04 15:53:00 +0800

    yolov5s_test

--- AlphaBot2/python/img_get.py
+++ AlphaBot2/python/img_get.py
@@ -1,5 +1,7 @@
 import cv2
+import time
 from picamera2 import Picamera2, Preview
+import torch
 
 def capture_image(picamera, interval=1):
     counter = 0
@@ -12,10 +14,14 @@ def main():
     picamera = Picamera2()
     picamera.configure(picamera.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
     picamera.start()
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 
     while True:
         im = picamera.capture_array()
-        cv2.imshow("Camera", im)
+        img = im[...,::-1] #bgr2rgb
+        result = model(img)
+        
+        cv2.imshow("Result", result)
         cv2.waitKey(1)
 
 if __name__ == "__main__":
